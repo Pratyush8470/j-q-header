41e33338d35dd1e82df6488d1061bd0ab5a1be0c
Pratyush8470 <pratyushbhatiya106@gmail.com>
2024-03-10 17:01:25 +0530

    J-query header(responsive)

---
+++ jq/scr.js
@@ -0,0 +1,55 @@
+$(window).on('load', function () {
+    $(".load-wrapper").delay(2000).fadeOut("slow")
+})
+
+
+$(document).ready(function () {
+    $('#bar').click(function () {
+        $('nav').slideToggle('fast');
+    })
+    $('#button').click(function () {
+        $('.colors').slideToggle('fast');
+    })
+    $('.color').click(function () {
+        let color = $(this).attr("id");
+        $("#themecss").attr("href", "css/" + color + ".css");
+    })
+})
+
+
+
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+var a = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('click');
+  cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function(){
+  cursor.classList.remove('click')
+  cursorinner.classList.remove('cursorinnerhover')
+});
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hover');
+  });
+})
